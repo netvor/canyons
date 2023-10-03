af52a09439947f49cfd5df0f1c4603456cf8ffc8
Floris van der Meijs [stewie] <floris@harpavieja.cz>
2023-10-03 13:13:14 +0200

    update html file

--- canyons.py
+++ canyons.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 
 print("Content-type: text/html\n")
-print("<html><body><h1>Canyons</h1>")
-print("<table border=1>")
-print("")
+#print("<html><body><h1>Canyons</h1>")
+#print("<table border=1>")
+#print("")
 
 import requests
 from lxml import html
+from lxml.html import builder as E
+from bs4 import BeautifulSoup as bs
 
 url='https://www.canyon.com/en-cz/gravel-bikes/adventure/grizl/?prefn1=pc_federung_rr&prefv1=No&prefn2=pc_rahmengroesse&prefv2=S&prefn3=pg_weight&prefv3=8%20-%209%20kg%7C9%20-%2010%20kg&srule=sort_price_ascending&format=ajax&showFilters=false&pmin=25.000%2C00&pmax=74.000%2C00'
 response = requests.get(url)
 page = html.fromstring(response.content)
+
+outpage = E.HTML(
+  E.BODY(
+  E.H1('Canyons'),
+  E.TABLE(
+)))
+table = outpage.xpath('//table')[0]
+table.set('border','1')
 
 for div in page.xpath('//li[@class="productGrid__listItem xlt-producttile"]/div'):
   for comparediv in div.xpath('.//div[@class="productTileCompare__wrapper" or @class="productTile__priceMonthly"]'):
@@ -21,9 +31,17 @@ for div in page.xpath('//li[@class="productGrid__listItem xlt-producttile"]/div'
     for sizebox in linkedpage.xpath('//div[@class="productConfiguration__variantType js-productConfigurationVariantType" and normalize-space()="S"]/..'):
       div.xpath('.//li[@class="productBadges__listItem productBadges__listItem--availability"]')[0].append(sizebox)
 
-  print("<tr><td>")
-  print(html.tostring(div, encoding=str))
-  print("</td></tr>")
-  print("")
+#  print("<tr><td>")
+  td = html.Element('td')
+  td.append(div)
+  tr = html.Element("tr")
+  tr.append(td)
+  tr.tail = "\n\n"
+  table.append(tr)
+#  print(html.tostring(div, encoding=str))
+#  print("</td></tr>")
+#  print("")
 
-print("</table>\n</body></html>")
+#print("</table>\n</body></html>")
+
+print(html.tostring(outpage, encoding=str))
